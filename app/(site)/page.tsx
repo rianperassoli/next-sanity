33c35e0bc8cd2c8c41d386a2db52797255a2a564
Rian Perassoli <rian.perassoli@gmail.com>
2023-08-01 17:28:01 -0300

    improving code

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,7 +8,7 @@ export default async function Home() {
   return (
     <div>
       <h1 className="text-7xl font-extrabold">
-        Hello I'm{" "}
+        Hello I&apos;m{" "}
         <span className="bg-gradient-to-r from-blue-400 via-yellow-200 to-green-400 bg-clip-text text-transparent">
           Rian Perassoli
         </span>
